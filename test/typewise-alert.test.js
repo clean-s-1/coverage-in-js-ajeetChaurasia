784275e1cdefb4a4e4cd29e596514545cc697230
AJEET KUMAR <cajeet@hotmail.com>
2022-07-07 14:01:21 +0530

    Add files via upload

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -1,10 +1,10 @@
 const alerts = require('../typewise-alert');
-const { expect } = require('chai');
+const {expect} = require('chai');
 const chai = require('chai');
 const spy = require('chai-spies');
 chai.use(spy);
 
-describe('typewise alerts', function () {
+describe('typewise alerts', function() {
   it('infers a value lower than the minimum as TOO_LOW', () => {
     expect(alerts.inferBreach(20, 50, 100)).equals('TOO_LOW');
   });
@@ -19,67 +19,67 @@ describe('typewise alerts', function () {
 
   it('classify temperature breach with PASSIVE COOLING type with value 0', () => {
     expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', 0)).equals(
-      'TOO_LOW'
+        'TOO_LOW',
     );
   });
 
   it('classify temperature breach with PASSIVE COOLING type with value -1', () => {
     expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', -1)).equals(
-      'TOO_LOW'
+        'TOO_LOW',
     );
   });
 
   it('classify temperature breach with PASSIVE COOLING type with value 20', () => {
     expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', 20)).equals(
-      'NORMAL'
+        'NORMAL',
     );
   });
 
   it('classify temperature breach with PASSIVE COOLING type with value 35', () => {
     expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', 35)).equals(
-      'TOO_HIGH'
+        'TOO_HIGH',
     );
   });
 
   it('classify temperature breach with PASSIVE COOLING type with value 49', () => {
     expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', 40)).equals(
-      'TOO_HIGH'
+        'TOO_HIGH',
     );
   });
 
   it('classify temperature breach with HI ACTIVE COOLING type with value -1', () => {
     expect(alerts.classifyTemperatureBreach('HI_ACTIVE_COOLING', -1)).equals(
-      'TOO_LOW'
+        'TOO_LOW',
     );
   });
 
   it('classify temperature breach with HI ACTIVE COOLING type with value 20', () => {
     expect(alerts.classifyTemperatureBreach('HI_ACTIVE_COOLING', 20)).equals(
-      'NORMAL'
+        'NORMAL',
     );
   });
 
   it('classify temperature breach with HI ACTIVE COOLING type with value 50', () => {
     expect(alerts.classifyTemperatureBreach('HI_ACTIVE_COOLING', 50)).equals(
-      'TOO_HIGH'
+        'TOO_HIGH',
     );
   });
 
   it('classify temperature breach with MED ACTIVE COOLING type with value -1', () => {
     expect(alerts.classifyTemperatureBreach('MED_ACTIVE_COOLING', -1)).equals(
-      'TOO_LOW'
+        'TOO_LOW',
     );
   });
 
   it('classify temperature breach with MED ACTIVE COOLING type with value 20', () => {
     expect(alerts.classifyTemperatureBreach('MED_ACTIVE_COOLING', 20)).equals(
-      'NORMAL'
+        'NORMAL',
     );
   });
 
   it('classify temperature breach with MED ACTIVE COOLING type with value 41', () => {
     expect(alerts.classifyTemperatureBreach('MED_ACTIVE_COOLING', 41)).equals(
-      'TOO_HIGH'
+        'TOO_HIGH',
     );
   });
 
@@ -90,14 +90,14 @@ describe('typewise alerts', function () {
 
   it('call the sendToMail function with TOO_LOW', () => {
     const breachType = 'TOO_LOW';
-    let spy = chai.spy(alerts.sendToEmail);
+    const spy = chai.spy(alerts.sendToEmail);
     spy(breachType);
     expect(spy).to.have.been.called.with('TOO_LOW');
   });
 
   it('call the sendToMail function with TOO_HIGH', () => {
     const breachType = 'TOO_HIGH';
-    let spy = chai.spy(alerts.sendToEmail);
+    const spy = chai.spy(alerts.sendToEmail);
     spy(breachType);
     expect(spy).to.have.been.called.with('TOO_HIGH');
   });
@@ -109,11 +109,11 @@ describe('typewise alerts', function () {
       coolingType: 'PASSIVE_COOLING',
     };
     const breachType = alerts.classifyTemperatureBreach(
-      mockBatteryChar.coolingType,
-      mockTemperatureInc
+        mockBatteryChar.coolingType,
+        mockTemperatureInc,
     );
     alerts.checkAndAlert(mockAlertTarget, mockBatteryChar, mockTemperatureInc);
-    let spy = chai.spy.on(alerts.sendToController);
+    const spy = chai.spy.on(alerts.sendToController);
     expect(spy(breachType)).to.have.been.called();
   });
 
@@ -124,15 +124,15 @@ describe('typewise alerts', function () {
       coolingType: 'HI_ACTIVE_COOLING',
     };
     const breachType2 = alerts.classifyTemperatureBreach(
-      mockBatteryChar2.coolingType,
-      mockTemperatureInc2
+        mockBatteryChar2.coolingType,
+        mockTemperatureInc2,
     );
     alerts.checkAndAlert(
-      mockAlertTarget2,
-      mockBatteryChar2,
-      mockTemperatureInc2
+        mockAlertTarget2,
+        mockBatteryChar2,
+        mockTemperatureInc2,
     );
-    let spy2 = chai.spy.on(alerts.sendToEmail);
-    expect(spy2).to.have.been.called();
+    const spy2 = chai.spy.on(alerts.sendToEmail);
+    expect(spy2(breachType2)).to.have.been.called();
   });
 });
